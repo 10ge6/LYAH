8957f93082534d3aa973ec403e49ce1604ac8bbe
10ge6 <dsf@jugemu.systems>
2022-10-01 15:57:53 -0300

    chapter 4 up to "let it be"

--- functionSyntax.hs
+++ functionSyntax.hs
@@ -17,23 +17,20 @@ charName 'c' = "Cecil"
 vectorAdd :: (Num a) => (a, a) -> (a, a) -> (a, a)
 vectorAdd (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 
--- Head function reimplementation...
+-- Some reimplementations...
 head' :: [a] -> a
 head' [] = error "NOPE"
 head' (x:_) = x
 
--- ...and Last (extra)
 last' :: [a] -> a                   -- Alternatively use monads(?) [a] -> Maybe a...
 last' [] = error "NOPE"             -- ... and [] = Nothing
 last' [x] = x                       -- Equivalent to (x:[])
 last' (_:xs) = last' xs
 
--- Length function reimplementation
 length' :: (Num b) => [a] -> b
 length' [] = 0                      -- 通称Edge condition（限りとしてとか）
 length' (_:x) = 1 + length' x       -- Notice the word consumption by _: until []
 
--- Sum function
 sum' :: (Num a) => [a] -> a
 sum' [] = 0
 sum' (x:xs) = x + sum' xs
@@ -42,4 +39,39 @@ sum' (x:xs) = x + sum' xs
 capital :: String -> String
 capital "" = "EMPTY"
 capital all@(x:xs) = "First letter of " ++ all ++ " is: " ++ [x]
+
+-- Guards
+densityTell :: (Fractional a, Ord a) => a -> a -> String     -- Alternatively declare typeclass of a to be RealFloat (subclass of Fractional, implies Ord)
+densityTell mass volume
+    | density < air    = "fly"
+    | density <= water = "float"
+    | otherwise              = "sink"
+    where density = mass / volume                            -- Names defined here are only visible to this function
+          (air, water) = (1.2, 1000.0)
+
+max' :: (Ord a) => a -> a -> a
+max' a b
+    | a > b     = a
+    | otherwise = b                 -- Writing these inline is unreadable (max' a b | a > b = a | otherwise = b) but possible
+
+compare' :: (Ord a) => a -> a -> Ordering
+a `compare'` b
+    | a > b     = GT
+    | a == b    = EQ
+    | otherwise = LT
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+    where (f:_) = firstname
+          (l:_) = lastname
+
+calcDensities :: (RealFloat a) => [(a, a)] -> [a]            -- Conversely, (Fractional a, Ord a) also works
+calcDensities xs = [density m v | (m, v) <- xs]
+    where density mass volume = mass / volume
+
+{-
+ - where bindings can also be nested. It's a common idiom to make a function
+ - and define some helper function in its where clause and then to give
+ - those functions helper functions as well, each with its own where clause.
+-}
 
